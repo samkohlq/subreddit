0afbd23e19500416eaaf01e029363765ad50ca58
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-17 18:08:33 +0800

    add comments to explain how store is created

--- src/configureStore.js
+++ src/configureStore.js
@@ -7,6 +7,8 @@ import rootReducer from "./reducers";
 const loggerMiddleware = createLogger();
 
 export default function configureStore(preloadedState) {
+  // returns redux store, holding complete state tree
+  // createStore takes in reducer argument, preloadedState, enhancer
   return createStore(
     rootReducer,
     preloadedState,
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -4,10 +4,14 @@ import { Provider } from "react-redux";
 import configureStore from "../configureStore";
 import AsyncApp from "./AsyncApp";
 
+// create store which holds complete state tree of app
 const store = configureStore();
+
 export default class Root extends Component {
   render() {
     return (
+      /* provider makes store available to any nested components
+      wrapped in connect() function */
       <Provider store={store}>
         <AsyncApp />
       </Provider>
