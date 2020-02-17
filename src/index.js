1f1a49751352b009feb39c4ae651c2976bd6167a
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-17 17:26:24 +0800

    amend README.md, add subreddit project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "core-js/stable";
+import React from "react";
+import { render } from "react-dom";
+import "regenerator-runtime/runtime";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from "./containers/Root";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Root />, document.getElementById("root"));
